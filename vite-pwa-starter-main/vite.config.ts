8a17e46fa1c143476bad6c65bcc660f88604afd5
jaysson18 <jahaka999@gmail.com>
2024-10-03 10:48:00 +0300

    update

--- vite-pwa-starter-main/vite.config.ts
+++ vite-pwa-starter-main/vite.config.ts
@@ -3,5 +3,15 @@ import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: './',
-  plugins: [VitePWA({registerType: 'autoUpdate'})],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf}'],
+      },
+    }),
+  ],
 });
