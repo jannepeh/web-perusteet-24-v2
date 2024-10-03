bb1cce2a9adbc96fcc6b6c0b7779cccf28e6ea0f
jaysson18 <jahaka999@gmail.com>
2024-10-03 11:08:47 +0300

    update

--- vite-pwa-starter-main/vite.config.ts
+++ vite-pwa-starter-main/vite.config.ts
@@ -12,6 +12,34 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf}'],
       },
+      manifest: {
+        name: 'Vite-PWA-example',
+        short_name: 'VitePWA',
+        description: 'example for vite pwa plugin',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
     }),
   ],
 });
